090db2b4ad0df91f1b52fac63c2c1836f89eeb53
SindhujaSrinivasan2025 <sindhujasrinivasanqa@gmail.com>
2026-01-16 18:29:44 +0530

    Add files via upload

---
+++ Day 2/12-oddnumbers.js
@@ -0,0 +1,9 @@
+let number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+console.log("Odd numbers are:");
+for(let i=0;i<20;i++)
+{
+    if(number[i]%2!=0)
+    {
+        console.log(number[i]);
+    }
+}
