f5627f0453ed87acde6d52d36bb2779b19191454
SindhujaSrinivasan2025 <sindhujasrinivasanqa@gmail.com>
2026-03-05 13:21:28 +0530

    Add files via upload

---
+++ Day 12/learntoReadEnv.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+import dotenv from "dotenv";
+let filename= process.env.envfile || 'qa'
+
+dotenv.config({path:`Data/${filename}.env`}) // setting the environment for testing purpose
+
+console.log(process.env.BaseURL);
+console.log(process.env.LF_Username);
+console.log(process.env.LF_Password);
+
+//console.log(process.env.USERNAME); // System environment
